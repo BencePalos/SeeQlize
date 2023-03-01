f9fafb6a93f08dbd9ba0911c172ca66473211a31
Palos Bence <palos.bence94@gmail.com>
2023-03-01 16:21:58 +0100

    starting to convert back-end to spring boot

---
+++ SeeQlize_backend/src/main/java/com/bencep/seeqlize_backend/SeeQlizeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bencep.seeqlize_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeQlizeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeQlizeBackendApplication.class, args);
+	}
+
+}
---
+++ SeeQlize_backend/src/test/java/com/bencep/seeqlize_backend/SeeQlizeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bencep.seeqlize_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeQlizeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
